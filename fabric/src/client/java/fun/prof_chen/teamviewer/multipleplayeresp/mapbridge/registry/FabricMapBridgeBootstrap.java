94cd5b5248d97792292b312bbe28d7078d42be25
jianuo <32106500027@e.gzhu.edu.cn>
2026-03-08 19:47:07 +0800

    旅行地图改进

--- fabric/src/client/java/fun/prof_chen/teamviewer/multipleplayeresp/mapbridge/registry/FabricMapBridgeBootstrap.java
+++ fabric/src/client/java/fun/prof_chen/teamviewer/multipleplayeresp/mapbridge/registry/FabricMapBridgeBootstrap.java
@@ -2,15 +2,33 @@ package fun.prof_chen.teamviewer.multipleplayeresp.mapbridge.registry;
 
 import fun.prof_chen.teamviewer.multipleplayeresp.mapbridge.provider.journey.JourneyMapBridgeModule;
 import fun.prof_chen.teamviewer.multipleplayeresp.mapbridge.provider.xaero.XaeroMapBridgeModule;
+import net.fabricmc.loader.api.FabricLoader;
 
 public final class FabricMapBridgeBootstrap {
+	private static final String JOURNEYMAP_MOD_ID = "journeymap";
+	private static final String XAERO_MINIMAP_MOD_ID = "xaerominimap";
+	private static final String XAERO_WORLDMAP_MOD_ID = "xaeroworldmap";
+
 	private FabricMapBridgeBootstrap() {
 	}
 
 	public static MapBridgeRegistry createRegistry() {
 		MapBridgeRegistry registry = new MapBridgeRegistry();
-		new XaeroMapBridgeModule().register(registry);
-		new JourneyMapBridgeModule().register(registry);
+		if (isXaeroInstalled()) {
+			new XaeroMapBridgeModule().register(registry);
+		}
+		if (isJourneyMapInstalled()) {
+			new JourneyMapBridgeModule().register(registry);
+		}
 		return registry;
+	}
+
+	private static boolean isXaeroInstalled() {
+		FabricLoader loader = FabricLoader.getInstance();
+		return loader.isModLoaded(XAERO_MINIMAP_MOD_ID) || loader.isModLoaded(XAERO_WORLDMAP_MOD_ID);
+	}
+
+	private static boolean isJourneyMapInstalled() {
+		return FabricLoader.getInstance().isModLoaded(JOURNEYMAP_MOD_ID);
 	}
 }
